4e786953e9dcda02835974dbe0022bf100107939
Sarjerao Sandbhor <40741655+SandbhorSarjerao@users.noreply.github.com>
2022-02-17 13:07:53 +0530

    Update Server.py

--- Server.py
+++ Server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, url_for, request, redirect
+import csv
 
 app = Flask(__name__)
 
@@ -18,6 +19,23 @@ def submit_form():
     else:
         return 'Something went wrong, Try Again!'
 
+# def write_to_file(data):
+#  with open('database.txt', mode='a') as database:
+#    email = data["email"]
+#    subject= data["subject"]
+#    message = data["message"]
+#    file = database.write(f'\n{email},{subject},{message}')
+
+def write_to_csv(data):
+  with open('database.csv', mode='a') as csv_database:
+    email = data["email"]
+    subject= data["subject"]
+    message = data["message"]
+    
+    csv_writer = csv.writer(csv_database, delimiter=',',
+                            quotechar='', quoting=csv.QUOTE_MINIMAL)
+    csv_writer.writerow([email, subject, message])
+    
 # @app.route('/<username>')                                 # Variable
 # def user(username=None):
 #     return render_template('about.html', name=username)
